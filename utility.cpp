c2f91b8777162cfc3ff68e248fbd28a4a7286bea
youngyangze <149575342+youngyangze@users.noreply.github.com>
2023-11-04 16:54:10 +0900

    utility

---
+++ utility.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace utility {
+    template <typename T>
+    int printVector(vector<T> vec, bool sorted = false) {
+        if (vec.empty()) {
+            return -1;
+        } else {
+            for (int i = 0; i < vec.size(); i++) {
+                cout << vec[i] << '\n';
+            }
+        }
+    }
+}
+
+int main() {
+
+
+    return 0;
+}
